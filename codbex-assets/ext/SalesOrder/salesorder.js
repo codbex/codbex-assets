e6a929178dda5dba77e37344c03182cc0cc4a750
alt-plus-f4 <valentin.assenov@abv.bg>
2024-10-01 17:32:38 +0000

    feat: Added SalesOrder and Waste Perspectives

--- codbex-assets/ext/SalesOrder/salesorder.js
+++ codbex-assets/ext/SalesOrder/salesorder.js
@@ -1,7 +1,7 @@
 const viewData = {
     id: 'assets-sales-order',
     label: 'Generate Sales Order',
-    link: '/services/web/codbex-assets/ext/SalesOrder/salesorder.html',
+    link: '/services/web/codbex-orders/gen/codbex-orders/ui/SalesOrder/index.html',
     perspective: 'Disposal',
     view: 'Disposal',
     type: 'entity',
