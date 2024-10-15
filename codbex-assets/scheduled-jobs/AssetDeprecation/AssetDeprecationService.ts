72c1f29ecb32e07c062568e7d3ebcc2183f7d10c
alt-plus-f4 <valentin.assenov@abv.bg>
2024-10-15 17:31:00 +0000

    fix: change wrong table name "ASSET" to "CODBEX_ASSET"

--- codbex-assets/scheduled-jobs/AssetDeprecation/AssetDeprecationService.ts
+++ codbex-assets/scheduled-jobs/AssetDeprecation/AssetDeprecationService.ts
@@ -2,7 +2,7 @@ import { update } from "sdk/db";
 
 export class AssetDeprecationService {
     public static updateAccumulatedValue(assetId: number, accumulatedValue: number) {
-        const sql = `UPDATE "ASSET" SET "ASSET_ACCUMULATEDVALUE" = ? WHERE "ASSET_ID" = ?`;
+        const sql = `UPDATE "CODBEX_ASSET" SET "ASSET_ACCUMULATEDVALUE" = ? WHERE "ASSET_ID" = ?`;
         const queryParameters = [accumulatedValue, assetId];
         update.execute(sql, queryParameters);
     }
