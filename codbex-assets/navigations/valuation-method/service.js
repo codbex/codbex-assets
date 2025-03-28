bffe1ad0ff83159fca3f87e2fc16e344388c993a
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-03-28 10:34:16 +0200

    change navigation order

--- codbex-assets/navigations/valuation-method/service.js
+++ codbex-assets/navigations/valuation-method/service.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'valuation-method-navigation',
     label: "Valuation Method",
     group: "reference data",
-    order: 2300,
+    order: 2400,
     link: "/services/web/codbex-assets/gen/codbex-assets/ui/ValuationMethod/index.html?embedded"
 };
 
